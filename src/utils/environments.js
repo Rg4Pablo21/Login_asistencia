530d7b5a7fc3de93a4400a5e6a3e470f855398a0
JBaires <juanjoseab@gmail.com>
2025-07-14 20:48:32 -0600

    se modifica la url de api

--- src/utils/environments.js
+++ src/utils/environments.js
@@ -1,5 +1,5 @@
 
-const API_BASE_URL = 'http://localhost:3000/api'
+const API_BASE_URL = 'https://control-asistencia-be.onrender.com/api'
 export const APIURLS = {
     base : API_BASE_URL,
     auth : {
