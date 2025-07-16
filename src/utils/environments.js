effa22dce452ef4537901d5a64bdf86e1fd5ddcb
JBaires <juanjoseab@gmail.com>
2025-07-15 21:40:48 -0600

    se cambia la url del backend

--- src/utils/environments.js
+++ src/utils/environments.js
@@ -1,6 +1,6 @@
 
-//const API_BASE_URL = 'https://control-asistencia-be.onrender.com/api'
-const API_BASE_URL = 'http://localhost:3000/api';
+const API_BASE_URL = 'https://control-asistencia-be.onrender.com/api'
+//const API_BASE_URL = 'http://localhost:3000/api';
 export const APIURLS = {
     base : API_BASE_URL,
     auth : {
